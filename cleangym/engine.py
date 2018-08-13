f7eaa105833a22f34264e7a4b0539f5d695e5ab5
Chris Beeler <chris.beeler@uoit.net>
2018-08-13 11:49:59 -0400

    Added isochoric heat capacity variable

--- cleangym/engine.py
+++ cleangym/engine.py
@@ -4,13 +4,14 @@ import gym
 class Engine(object):
     def __init__(self, Ti=300.0, Vi=0.001, Tc=300.0, Th=500.0, dV=0.0002):
         self.N = 1.0/22.4 # 1 mole occupies 22.4 L at STP
-        self.c = 5.0/3.0  # Type of gas 
         self.kB = 1.38064852e-23  #Boltzmann constant
         self.NA = 6.0221409e+23   #Avogadro's number
 
         self.J_to_kJ = 0.001
 
         self.R = self.kB * self.NA * self.J_to_kJ # Units of kJ/K/mol
+
+        self.Cv = (3.0/2.0) * self.N * self.R # Cv for this type of gas
 
         self.Vmin = 0.0001 # m**3
         self.Vmax = 0.001
@@ -30,7 +31,7 @@ class Engine(object):
 
     def __update_equations_of_state(self):
         self.P = self.N * self.T * self.R / self.V
-        self.U = 3./2. * self.N * self.NA * self.kB * self.T
+        self.U = self.Cv * self.T
 
     def reset(self):
         self.T = self.Ti
@@ -51,7 +52,7 @@ class Engine(object):
         self.T = T * ((V/self.V) ** (2.0/3.0))
         self.__update_equations_of_state()
 
-        dW = -(3.0/2.0) * self.N * self.R * (self.T - T)
+        dW = -self.Cv * (self.T - T)
         dQ = 0.0
         return self.T, self.V, dW, dQ
 
@@ -64,7 +65,7 @@ class Engine(object):
         self.T = T * ((V/self.V) ** (2.0/3.0))
         self.__update_equations_of_state()
 
-        dW = -(3.0/2.0) * self.N * self.R * (self.T - T)
+        dW = -self.Cv * (self.T - T)
         dQ = 0.0
         return self.T, self.V, dW, dQ
 
@@ -109,7 +110,7 @@ class Engine(object):
 
         dW = 0.0
         if T < self.Th:
-            dQ = (3.0/2.0) * self.N * self.R * (self.Th - T)
+            dQ = self.Cv * (self.Th - T)
         else:
             dQ = 0.0
         
@@ -127,7 +128,7 @@ class Engine(object):
         dW = self.N * self.R * self.T * np.log(self.V / V)
         
         if T < self.Th:
-            dQ = (3.0/2.0) * self.N * self.R * (self.Th - T)
+            dQ = self.Cv * (self.Th - T)
         else:
             dQ = 0
             
@@ -146,7 +147,7 @@ class Engine(object):
         dW = self.N * self.R * self.T * np.log(self.V / V)
         
         if T < self.Th:
-            dQ = dW + (3.0/2.0) * self.N * self.R * (self.Th - T)
+            dQ = dW + self.Cv * (self.Th - T)
         else:
             dQ = dW
         return self.T, self.V, dW, dQ
