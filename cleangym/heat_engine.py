43500183a4735404b5f05fc78253393844f2aa74
Kyle Mills <kyle.mills@uoit.net>
2018-09-20 16:16:14 -0700

    updated heat engine rendering

--- cleangym/heat_engine.py
+++ cleangym/heat_engine.py
@@ -3,7 +3,7 @@ import gym
 import gym.spaces
 from cleangym.engine import Engine
 import matplotlib.pyplot as plt
-
+from matplotlib.patches import Rectangle
 class HeatEngineEnv(gym.Env):
     def __init__(self, *args, **kwargs):
         self.engine = Engine(*args, **kwargs)
@@ -11,29 +11,47 @@ class HeatEngineEnv(gym.Env):
         self.Q = []
         self.W = []
         self._first_render = True
+        self._plot_data_persistent = {"r":[]}
+        self._max_episode_steps = 100000
 
     def reset(self):
         self.engine.reset()
+        self.done = False
         self.Q = []
         self.W = []
         T = (self.engine.T - self.engine.Tmin) / (self.engine.Tmax - self.engine.Tmin)
         V = (self.engine.V - self.engine.Vmin) / (self.engine.Vmax - self.engine.Vmin)
         self._plot_data = {"P" : [self.engine.P], "V": [self.engine.V], "r":[0] }
         self._first_render=True
+        self._plot_data_persistent["r"].append(0)
         return np.array([T, V])
 
 
     def render(self, mode='plot'):
+        pv_points_to_plot = 12
         if self._first_render:
             plt.xkcd()
             plt.close('all')
             plt.ion()
-            self._plot_fig, self._plot_axs = plt.subplots(1,2,figsize=(8,3))
-            self._plot_li0, = self._plot_axs[0].plot(self._plot_data['V'], self._plot_data['P'], 'o--')
+            self._plot_fig, self._plot_axs = plt.subplots(1,2,figsize=(12,6))
+            self._plot_li0_masks = []
+            self._plot_li0, = self._plot_axs[0].plot(self._plot_data['V'][-6:], self._plot_data['P'][-6:])
+            for i in range(pv_points_to_plot):
+                self._plot_li0_masks.append(
+                            self._plot_axs[0].plot(self._plot_data['V'][-pv_points_to_plot:-pv_points_to_plot+i], self._plot_data['P'][-pv_points_to_plot:-pv_points_to_plot+i],
+                            alpha=1.,#/pv_points_to_plot,
+                            color='blue', linewidth=3)[0]
+                     )
+
             self._plot_li1, = self._plot_axs[1].plot(np.arange(len(self._plot_data["r"])), self._plot_data["r"], 'g-')
-            self._plot_axs[1].axhline(y=self.efficiency, color='red', ls='--') #np.arange(len(self._plot_data["r"])), np.zeros_like(np.arange(len(self._plot_data["r"])))+self.efficiency, 'r-')
-            self._plot_axs[0].set_xlabel("Volume")
-            self._plot_axs[0].set_ylabel("Pressure")
+            self._plot_axs[1].axhline(y=self.efficiency, color='red', ls='--')
+            self._plot_axs[0].set_xlabel("Volume [L]")
+            self._plot_axs[0].set_ylabel("Pressure ")
+            vr = abs(self.engine.Vmin-self.engine.Vmax)
+            pr = abs(self.engine.Pmin-self.engine.Pmax)
+            self._plot_axs[0].add_patch(Rectangle((self.engine.Vmin*1000, self.engine.Pmin), vr*1000., pr, color='#DDDDDD'))
+            self._plot_axs[0].set_xlim([(self.engine.Vmin-0.1*vr)*1000, (self.engine.Vmax+0.1*vr)*1000])
+            self._plot_axs[0].set_ylim([self.engine.Pmin-0.1*pr, self.engine.Pmax+0.1*pr])
             self._plot_axs[1].set_xlabel("Step")
             self._plot_axs[1].set_ylabel("Efficiency")
             plt.tight_layout()
@@ -42,11 +60,16 @@ class HeatEngineEnv(gym.Env):
             self._first_render = False
 
         else:
-            self._plot_li0.set_xdata(self._plot_data['V'])
-            self._plot_li0.set_ydata(self._plot_data['P'])
+            self._plot_li0.set_xdata(self._plot_data['V'][-6:])
+            self._plot_li0.set_ydata(self._plot_data['P'][-6:])
             self._plot_li1.set_xdata(np.arange(len(self._plot_data["r"])))
             self._plot_li1.set_ydata(self._plot_data["r"])
-            for ax in [self._plot_axs[0], self._plot_axs[1]]:
+            for  i in range(len(self._plot_li0_masks)):
+                self._plot_li0_masks[i].set_xdata(self._plot_data['V'][-pv_points_to_plot:-pv_points_to_plot+i])
+                self._plot_li0_masks[i].set_ydata(self._plot_data['P'][-pv_points_to_plot:-pv_points_to_plot+i])
+
+
+            for ax in [self._plot_axs[1]]:
                 ax.relim()
                 ax.autoscale_view(True, True, True)
             self._plot_fig.canvas.draw()
@@ -63,7 +86,8 @@ class HeatEngineEnv(gym.Env):
         T = (self.engine.T - self.engine.Tmin) / (self.engine.Tmax - self.engine.Tmin)
         V = (self.engine.V - self.engine.Vmin) / (self.engine.Vmax - self.engine.Vmin)
         self._plot_data['P'].append(self.engine.P)
-        self._plot_data['V'].append(V)
+        self._plot_data['V'].append(self.engine.V*1000.)
         self._plot_data['r'].append(r)
+        self._plot_data_persistent['r'][-1] += r
         return np.array([T, V]), r, self.done, np.array([self.engine.T, self.engine.V, self.engine.P])
 
