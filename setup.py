f153a672316bd8edc334333cac93f560130d0b24
Kyle Mills <kyle.mills@uoit.net>
2018-11-08 11:46:59 -0800

    removed old dependency from setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup, find_packages
 import sys
 
-setup(name='carnotgym',
-      py_modules=['cleangym'],
+setup(name='heatenginegym',
+      py_modules=['heatenginegym'],
       install_requires=[
           'gym',
           'scipy',
-          'progressbar2',
           'tensorflow>=1.4.0',
           'matplotlib',
           'numpy',
       ],
-      description='CLEAN implementation of Carnot (and various other thermodynamic engine) gym environments.',
+      description='Implementation of heat engine simulations in the OpenAI Gym environment framework.',
       author='CLEAN',
-      url='https://github.com/CLEANit/carnot',
-      version='0.4')
+      url='https://github.com/CLEANit/heatenginegym',
+      version='1.0')
